1643a965d4e869535224d4dc7494744a020a5635
Robert <osipovitchr@gmail.com>
2022-05-19 12:21:08 -0400

    added server side validation for email addresses

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,25 @@
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
 const User = require('../models/users')
 
 
 module.exports = {
    createUser: async (args, req) => {
        const {email, password} = args.userInput
+       const errors = []
+       if(!validator.isEmail(email)) {
+           errors.push({message: 'E-mail is invalid'})
+       }
+
+       if(validator.isEmpty(password) || !validator.isLength(password, {min: 1})) {
+            errors.push({message: 'Must enter a password'})
+       }
+
+       if(errors.length > 0) {
+            const error = new Error('Invalid Input')
+            throw error
+       }
+
        const existingUser = await User.findOne({email: email})
        if (existingUser) {
            const error = new Error('User exist already')
